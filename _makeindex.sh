651e3b938cdbc6022a92461456e1accb7e54c547
nallar.me Jenkins <jenkins@nallar.me>
2017-01-19 22:15:32 +0000

    2017-01-19:22:15:32

--- _makeindex.sh
+++ _makeindex.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-LANG=c ls | grep "^[^_]" | perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "\n<li><a href=\"./$_\">$_</a></li>";} print "\n</ul></body></html>"' > index.html
+LANG=c ls | grep -v "^_" | grep -v "^CNAME$" | perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "\n<li><a href=\"./$_\">$_</a></li>";} print "\n</ul></body></html>"' > index.html
